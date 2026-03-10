9afd7ed423a41b73cf677ce59816c8c77100328c
HappyBoi04092004 <163809990+HappyBoi04092004@users.noreply.github.com>
2026-03-10 09:00:10 +0700

    test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,24 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+// import { BrowserRouter, Routes, Route } from "react-router-dom"
 
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/" element={<h1>Bookstore App 🚀</h1>} />
+//       </Routes>
+//     </BrowserRouter>
+//   )
+// }
+
+// export default App
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<h1>Home Page</h1>} />
-      </Routes>
-    </BrowserRouter>
-  )
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Bookstore App 🚀
+      </h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
